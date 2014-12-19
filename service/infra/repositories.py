311b463c993a6dfdde1ab40fab4bea8ab8cbcfd9
novy154 <slnowak154@gmail.com>
2014-12-19 20:00:26 +0100

    in memory repository implementation

--- service/infra/repositories.py
+++ service/infra/repositories.py
@@ -1,6 +1,6 @@
 __author__ = 'novy'
 
-# todo: implement
+
 class InMemoryRepository(object):
     def __init__(self):
         super().__init__()
@@ -10,10 +10,23 @@ class InMemoryRepository(object):
         self.events.append(event)
 
     def get_latest(self):
-        pass
+        inverted_list = inverse_list(self.events)
+        return top_k_from(inverted_list)
 
-    def get_latest_by_category(self):
-        pass
+    def get_latest_by_category(self, category):
+        inverted_list = inverse_list(self.events)
+        items_matching_category = [event for event in inverted_list if event.matches_category(category)]
+        return top_k_from(items_matching_category)
 
-    def get_latest_by_person(self):
-        pass
+    def get_latest_by_person(self, person):
+        inverted_list = inverse_list(self.events)
+        items_matching_person = [event for event in inverted_list if event.matches_person(person)]
+        return top_k_from(items_matching_person)
+
+
+def inverse_list(list_to_inverse):
+    return list_to_inverse[::-1]
+
+
+def top_k_from(list_to_limit, k=10):
+    return list_to_limit[:k]
--- service/infra/test_repositories.py
+++ service/infra/test_repositories.py
@@ -1,3 +1,4 @@
+import datetime
 from unittest import TestCase
 from unittest.mock import Mock
 from service.infra.repositories import InMemoryRepository
@@ -10,34 +11,26 @@ class TestInMemoryRepository(TestCase):
     def setUp(self):
         super().setUp()
         self.events = [
-            self._create_event_mock({"category1", "category2", "default"}, {"all", "john"}),
-            self._create_event_mock({"default"}, {"all", "no_one"}),
-            self._create_event_mock({"category1", "cat2", "default"}, {"all"}),
-            self._create_event_mock({"cat3", "cat2", "cat1", "default"}, {"no_one", "all"}),
-            self._create_event_mock({"category1", "cat", "default"}, {"all"}),
-            self._create_event_mock({"category", "default"}, {"john", "all"}),
-            self._create_event_mock({"category1", "category2", "default"}, {"mike", "john", "all"}),
-            self._create_event_mock({"category1", "category2", "category3", "default"}, {"mike", "all"}),
-            self._create_event_mock({"category3", "default"}, {"jack", "mike", "all"}),
-            self._create_event_mock({"without_default"}, {"nobody"}),
-            self._create_event_mock({"category", "default"}, {"random person", "all"}),
-            self._create_event_mock({"category", "category2", "default"}, {"all", "john"}),
-            self._create_event_mock({"category1", "category2", "default"}, {"all"}),
+            self._create_event({"category1", "category2", "default"}, {"all", "john"}),
+            self._create_event({"default"}, {"all", "no_one"}),
+            self._create_event({"category1", "cat2", "default"}, {"all"}),
+            self._create_event({"cat3", "cat2", "cat1", "default"}, {"no_one", "all"}),
+            self._create_event({"category1", "cat", "default"}, {"all"}),
+            self._create_event({"category", "default"}, {"john", "all"}),
+            self._create_event({"category1", "category2", "default"}, {"mike", "john", "all"}),
+            self._create_event({"category1", "category2", "category3", "default"}, {"mike", "all"}),
+            self._create_event({"category3", "default"}, {"jack", "mike", "all"}),
+            self._create_event({"without_default"}, {"nobody"}),
+            self._create_event({"category", "default"}, {"random person", "all"}),
+            self._create_event({"category", "category2", "default"}, {"all", "john"}),
+            self._create_event({"category1", "category2", "default"}, {"all"}),
         ]
 
         self.object_under_test = InMemoryRepository()
         self.object_under_test.events = self.events
 
-    def _create_event_mock(self, categories, people):
-        event = Mock(Event)
-
-        category_matches = lambda category: (category in categories)
-        person_matches = lambda person: (person in people)
-
-        event.category_matches = category_matches
-        event.person_matches = person_matches
-
-        return event
+    def _create_event(self, categories, people):
+        return Event(creation_time=Mock(datetime), content="", categories=categories, people=people)
 
     def test_should_return_recent_ten_events(self):
         expected_result = [self.events[12], self.events[11], self.events[10], self.events[9], self.events[8],
@@ -67,8 +60,8 @@ class TestInMemoryRepository(TestCase):
         self.assertEqual(actual_result, expected_result)
 
     def test_should_return_less_than_ten_events_searching_by_category_given_less_matching(self):
-        expected_result = [self.events[12], self.events[7], self.events[6], self.events[4], self.events[6],
-                           self.events[2], self.events[0]
+        expected_result = [self.events[12], self.events[7], self.events[6],
+                           self.events[4], self.events[2], self.events[0]
                            ]
 
         actual_result = self.object_under_test.get_latest_by_category("category1")
@@ -87,5 +80,5 @@ class TestInMemoryRepository(TestCase):
         self.assertEqual(self.object_under_test.get_latest_by_category("fake category"), [])
 
     def test_should_return_empty_list_given_not_matching_person(self):
-
+        print (self.events[0].matches_category("category1"))
         self.assertEqual(self.object_under_test.get_latest_by_person("fake person"), [])
